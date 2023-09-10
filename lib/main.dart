81288acc3c1eeaf36a3a71e5dc44f4ec43721413
IuryC <iurycauan0352@hotmail.com>
2023-09-10 12:01:49 -0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'myapp.dart';
+
 void main() {
 
-  runApp(const Myapp());
+  runApp(const MyApp());
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Material(
-        child: Center
-          (child: Text("O povo quer gozar")
-        )
-    );
-  }
 
-}
