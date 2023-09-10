9df3503a1f1717b3727c978ad1ce11e9300b43db
IuryC <iurycauan0352@hotmail.com>
2023-09-10 19:30:16 -0300

    ok

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,25 +1,64 @@
-import 'package:curso_flutter/home_controller.dart';
+// import 'package:curso_flutter/home_controller.dart';
+// import 'package:flutter/material.dart';
+//
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     var controller = HomeController.of(context);
+//
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Home'),
+//       ),
+//       body: Center(
+//         child: Text('O povo quer gozar ${controller.value}'),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Icon(Icons.add),
+//         onPressed: () {
+//           controller.increment();
+//         },
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    var controller = HomeController.of(context);
 
+    final names = ["Luffy", "Zoro", "Sanji", "Nami", "Usopp", "Chopper", "Nico Robin", "Broook", "Franky", "Jinbei"];
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('Tripulantes Mugiwara'),
       ),
-      body: Center(
-        child: Text('O povo quer gozar ${controller.value}'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          controller.increment();
-        },
+      body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: ListView.builder(
+            itemCount: names.length,
+            itemBuilder: (context, index) {
+              final name = names[index];
+              return ListTile(
+                title: Text(name),
+              );
+            },
+          )
+
       ),
     );
   }
